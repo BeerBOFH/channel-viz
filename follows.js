777dba144080e18100ead3363dc81304be3e9b3e
BeerBOFH <BeerBOFH@users.noreply.github.com>
2015-08-31 00:30:13 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= 'XXXy5hhHv4I6YNI9EvJ6T00dZKn9u4wPE6tLREPkm6ZU35xx', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1197161876], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Xively Arduino Brew Temp', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
